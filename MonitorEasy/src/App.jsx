b6542e0e5616ed5045cc000eaf6e1c9538a04c11
KuhakoX <caiocampomendes@hotmail.com>
2026-06-01 10:19:17 -0300

    Criação do front end do componente de Login, com opção de monitor e aluno

---
+++ MonitorEasy/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Login from './componentes/login'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
+
